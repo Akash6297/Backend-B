a7e6d3823f6cda23eb32d0f0d4b32a5b65af9118
Akash Mandal <akashmandal6297@gmail.com>
2023-07-20 19:31:55 +0530

    first commit

---
+++ contact.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+
+const ContactMessageSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  message: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const ContactMessage = mongoose.model('ContactMessage', ContactMessageSchema);
+
+// Route to handle contact form submissions
+router.post('/contact', async (req, res) => {
+  const { name, email, message } = req.body;
+
+  try {
+    const newMessage = new ContactMessage({ name, email, message });
+    await newMessage.save();
+    res.status(201).json({ message: 'Message sent successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
+module.exports = router;
---
+++ index.js
@@ -0,0 +1,86 @@
+// index.js
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const bcrypt = require('bcryptjs');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+// MongoDB Connection
+mongoose.connect('mongodb+srv://akashmandal6297:6a14zm3h0QcHFjUt@cluster0.xwdlcdu.mongodb.net/?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+
+});
+
+const UserSchema = new mongoose.Schema({
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+});
+
+
+const User = mongoose.model('User', UserSchema);
+
+// Route to handle user registration
+app.post('/api/signup', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Check if the user already exists
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ error: 'User already exists' });
+    }
+
+    // Hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create the new user
+    user = new User({ email, password: hashedPassword });
+    await user.save();
+
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
+// Route to handle user sign-in
+app.post('/api/signin', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Check if the user exists
+    let user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ error: 'Invalid email or password' });
+    }
+
+    // Check the password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ error: 'Invalid email or password' });
+    }
+
+    res.status(200).json({ message: 'Sign in successful' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+// Import and use the contact route
+const contactRoute = require('./contact');
+app.use('/api', contactRoute);
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
